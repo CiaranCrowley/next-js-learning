761b36427cfc9f6ebb7eae37718b260055138604
Ciarán Crowley <ciarancrowley37@gmail.com>
2023-06-17 19:05:25 +0100

    Programmatic Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Image from 'next/image';
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 export default function Home() {
+	const router = useRouter();
+	const [location, setLocation] = useState('');
+
 	return (
 		<main className="bg-gray-100 min-h-screen w-screen">
 			<main className="max-w-screen-2xl m-auto bg-white">
@@ -35,8 +41,16 @@ export default function Home() {
 									className="rounded  mr-3 p-2 w-[450px]"
 									type="text"
 									placeholder="State, city or town"
+									value={location}
+									onChange={(e) => setLocation(e.target.value)}
 								/>
-								<button className="rounded bg-red-600 px-9 py-2 text-white">
+								<button
+									className="rounded bg-red-600 px-9 py-2 text-white"
+									onClick={() => {
+										if (location === 'banana') return;
+										router.push('/search');
+									}}
+								>
 									Let's go
 								</button>
 							</div>
