cbaa900f3b28a4c0691240316d5089ee6b19b869
Ciarán Crowley <ciarancrowley37@gmail.com>
2023-06-23 23:23:50 +0100

    Now fetching individual restaurants.

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -8,12 +8,14 @@ export const metadata = {
 
 export default function RestaurantLayout({
 	children,
+	params,
 }: {
 	children: React.ReactNode;
+	params: { slug: string };
 }) {
 	return (
 		<main>
-			<Header />
+			<Header name={params.slug}/>
 			<div className="0 m-auto -mt-11 flex w-2/3 items-start justify-between">
 				{children}
 			</div>
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -7,16 +7,53 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationsCard from "./components/ReservationsCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+	id: number;
+	name: string;
+	description: string;
+	images: string[];
+	slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+	const restaurant = await prisma.restaurant.findUnique({
+		where: {
+			slug,
+		},
+		select: {
+			id: true,
+			name: true,
+			description: true,
+			images: true,
+			slug: true,
+		},
+	});
+
+	if (!restaurant) throw new Error();
+
+	return restaurant;
+};
+
+export default async function RestaurantDetails({
+	params,
+}: {
+	params: { slug: string };
+}) {
+	const restaurant = await fetchRestaurantBySlug(params.slug);
+	// console.log(restaurant);
+
 	return (
 		<>
 			<div className="w-[70%] rounded bg-white p-3 shadow">
-				<RestaurantNavBar />
-				<Title />
+				<RestaurantNavBar slug={restaurant.slug} />
+				<Title name={restaurant.name} />
 				<Rating />
-				<Description />
-				<Images />
+				<Description desc={restaurant.description} />
+				<Images images={restaurant.images}/>
 				<Reviews />
 			</div>
 			<div className="text-reg relative w-[27%]">
