acb3562e18c4482916a9bcf38fc17cba613a7ef8
Ciarán Crowley <ciarancrowley37@gmail.com>
2023-06-25 12:51:16 +0100

    Clicking enter on search field triggers searching

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -7,14 +7,27 @@ export default function SearchBar() {
 	const router = useRouter();
 	const [location, setLocation] = useState("");
 
+	const something = (event: { keyCode: number }) => {
+		if (event.keyCode === 13) {
+			if (location === "") return;
+			router.push(`/search?city=${location}`);
+			setLocation("");
+		}
+	};
+
 	return (
 		<div className="m-auto flex justify-center py-3 text-left text-lg">
 			<input
+				id="search-bar-input"
 				className="mr-3  w-[450px] rounded p-2"
 				type="text"
 				placeholder="State, city or town"
 				value={location}
-				onChange={(e) => setLocation(e.target.value)}
+				onChange={(e) => {
+					setLocation(e.target.value);
+					console.log(e.target.value);
+				}}
+				onKeyDown={(e) => something(e)}
 			/>
 			<button
 				className="rounded bg-red-600 px-9 py-2 text-white"
