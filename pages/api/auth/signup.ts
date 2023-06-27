4fe437d0e788e130adf4de3737bf96c3be0236bd
Ciarán Crowley <ciarancrowley37@gmail.com>
2023-06-27 11:07:19 +0100

    Created endpoint

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	if (req.method === "POST") {
+		res.status(200).json({
+			hello: "there",
+		});
+	} else {
+		res.status(200).json({
+			hello: "world",
+		});
+	}
+}
