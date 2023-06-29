8fb544a888fe7f4c79354ea9d5855d6d87101b41
Ciarán Crowley <ciarancrowley37@gmail.com>
2023-06-29 16:07:36 +0100

    Hide auth buttons while loading

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+"use client";
+
 import Link from "next/link";
 import AuthModal from "./AuthModal";
+import { useContext } from "react";
+import { AuthenticationContext } from "../context/AuthContext";
 
 export default function NavBar() {
+	const { data, loading } = useContext(AuthenticationContext);
+
 	return (
 		<nav className="flex justify-between bg-white p-2">
 			<Link href="/" className="text-2xl font-bold text-gray-700">
 				OpenTable
 			</Link>
 			<div>
-				<div className="flex">
-					<AuthModal isSignedIn={true} />
-					<AuthModal isSignedIn={false} />
-				</div>
+				{loading ? null : (
+					<div className="flex">
+						{data ? (
+							<button className="mr-3 rounded border bg-blue-400 p-1  px-4 text-white ">
+								Logout
+							</button>
+						) : (
+							<>
+								<AuthModal isSignedIn={true} />
+								<AuthModal isSignedIn={false} />
+							</>
+						)}
+					</div>
+				)}
 			</div>
 		</nav>
 	);
