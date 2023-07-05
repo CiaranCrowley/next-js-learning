444d57af3f755edc3c08d377cbf648a5bef187e7
Crowley, Ciaran <Ciaran.Crowley@Dell.com>
2023-07-05 14:34:10 +0100

    Working Inputs

--- app/reserve/[slug]/components/ReservationForm.tsx
+++ app/reserve/[slug]/components/ReservationForm.tsx
@@ -1,45 +1,87 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 export default function ReservationForm() {
+	const [disabled, setDisabled] = useState(true);
+	const [inputs, setInputs] = useState({
+		first_name: "",
+		last_name: "",
+		phone: "",
+		email: "",
+		occasion: "",
+		requests: "",
+	});
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputs({
+			...inputs,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	useEffect(() => {
+		if (inputs.first_name && inputs.last_name && inputs.phone && inputs.email) {
+			return setDisabled(false)
+		} else return setDisabled(true)
+	}, [inputs]);
+
 	return (
-		<div className="mt-10 flex flex-wrap justify-between w-[660px]">
+		<div className="mt-10 flex w-[660px] flex-wrap justify-between">
 			<input
 				type="text"
-				className="border rounded p-3 w-80 mb-4"
+				className="mb-4 w-80 rounded border p-3"
 				placeholder="First name"
+				name="first_name"
+				value={inputs.first_name}
+				onChange={handleChange}
 			/>
 			<input
 				type="text"
-				className="border rounded p-3 w-80 mb-4"
+				className="mb-4 w-80 rounded border p-3"
 				placeholder="Last name"
+				name="last_name"
+				value={inputs.last_name}
+				onChange={handleChange}
 			/>
 			<input
 				type="text"
-				className="border rounded p-3 w-80 mb-4"
+				className="mb-4 w-80 rounded border p-3"
 				placeholder="Phone number"
+				name="phone"
+				value={inputs.phone}
+				onChange={handleChange}
 			/>
 			<input
 				type="text"
-				className="border rounded p-3 w-80 mb-4"
+				className="mb-4 w-80 rounded border p-3"
 				placeholder="Email"
+				name="email"
+				value={inputs.email}
+				onChange={handleChange}
 			/>
 			<input
 				type="text"
-				className="border rounded p-3 w-80 mb-4"
+				className="mb-4 w-80 rounded border p-3"
 				placeholder="Occasion (optional)"
+				name="occasion"
+				value={inputs.occasion}
+				onChange={handleChange}
 			/>
 			<input
 				type="text"
-				className="border rounded p-3 w-80 mb-4"
+				className="mb-4 w-80 rounded border p-3"
 				placeholder="Requests (optional)"
+				name="requests"
+				value={inputs.requests}
+				onChange={handleChange}
 			/>
-			<button className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300">
+			<button disabled={disabled} className="w-full rounded bg-red-600 p-3 font-bold text-white disabled:bg-gray-300">
 				Complete reservation
 			</button>
 			<p className="mt-4 text-sm">
-				By clicking “Complete reservation” you agree to the OpenTable Terms
-				of Use and Privacy Policy. Standard text message rates may apply.
-				You may opt out of receiving text messages at any time.
+				By clicking “Complete reservation” you agree to the OpenTable Terms of Use and Privacy Policy. Standard text message
+				rates may apply. You may opt out of receiving text messages at any time.
 			</p>
 		</div>
 	);
