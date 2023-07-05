ff8588f09e59f206e5941b8779e54c4abf65cd43
Crowley, Ciaran <Ciaran.Crowley@Dell.com>
2023-07-05 11:26:33 +0100

    Reservation responsive header

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import Header from "./components/Header";
 import ReservationForm from "./components/ReservationForm";
+import { PrismaClient } from "@prisma/client";
+import { notFound } from "next/navigation";
 
 export const metadata = {
 	title: "Reserve | OpenTable",
 	description: "4321",
 };
 
-export default function Reserve() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+	const response = await prisma.restaurant.findUnique({
+		where: {
+			slug,
+		},
+	});
+
+	if (!response) {
+		return notFound();
+	}
+
+	return response;
+};
+
+export default async function Reserve({ params }: { params: { slug: string } }) {
+	const restaurant = await fetchRestaurantBySlug(params.slug);
 	return (
 		<div className="h-screen border-t">
 			<div className="m-auto w-3/5 py-9">
-				<Header />
+				<Header image={restaurant.main_img} name={restaurant.name}/>
 				<ReservationForm />
 			</div>
 		</div>
